93cbc2ca9d078709d2b8d86ce80c3513aa56db27
asegar01 <asegar01@ucm.es>
2022-12-02 00:19:34 +0100

    Buoyancy

---
+++ skeleton/BuoyancyForceGenerator.cpp
@@ -0,0 +1,22 @@
+#include "BuoyancyForceGenerator.h"
+
+void BuoyancyForceGenerator::updateForce(Particle* particle, double t) 
+{
+	// Check that the particle has Finite Mass
+	if (fabs(particle->getInverseMass()) < 1e-10) return;
+
+	// Compute the buoyancy force
+	const float h = particle->getPosition().p.y;
+	const float h0 = _liquid_particle->getPosition().p.y;
+	Vector3 f(0, 0, 0);
+
+	float immersed = 0.0;
+	if (h - h0 > _height * 0.5) immersed = 0.0;
+	else if (h0 - h > _height * 0.5) immersed = 1.0; // Totally immersed
+	else immersed = (h0 - h) / _height + 0.5;
+
+	f.y = _liquid_density * _volume * immersed * _gravity;
+
+	// Apply the buoyancy force
+	particle->addForce(f);
+}
---
+++ skeleton/BuoyancyForceGenerator.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "core.hpp"
+#include "ForceGenerator.h"
+
+class BuoyancyForceGenerator : public ForceGenerator
+{
+public:
+	BuoyancyForceGenerator(float h, float v, float d) : _height(h), _volume(v), _liquid_density(d)
+	{ _liquid_particle = new Particle(PLANE, { 0.0, 30.0, 0.0 }, { 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0 }, 0.0f, 0.0f, 1e6); }
+
+	virtual void updateForce(Particle* particle, double t);
+
+	inline void setVolume(float v) { _volume = v; }
+	inline float getVolume() { return _volume; }
+
+	virtual ~BuoyancyForceGenerator() { delete _liquid_particle; }
+
+protected:
+	float _height;
+	float _volume;
+	float _liquid_density;
+	float _gravity = 9.8;
+
+	Particle* _liquid_particle; // For representation
+};
